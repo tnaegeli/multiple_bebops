bad2e3231853ca3ce419d2d67011208d024a803b
Radulovic Nikola <40368376+nikolardlvc@users.noreply.github.com>
2020-05-18 09:21:29 +0200

    update

--- shortpress_3.sh
+++ shortpress_3.sh
@@ -1,28 +1,34 @@
 #!/bin/sh
 #Set the SSID, Password and IP
-SSID='ssid'	# Change this to your SSID
-PW='password' 		# Change this to the password of your wifi network
-IP=192.168.1.101 	# Change this to the desired Bebop IP	
+IP=192.168.1.10         # Change this to the desired Bebop IP   
+MASK=255.255.0.0
+
+CONFIG='network={\n
+        ssid="ice0nfire-5G" \n
+        key_mgmt=NONE \n
+}
+'
+
+bcmwl band a
 
 #Connect to defined Network
 BLDC_Test_Bench -M 2 >/dev/null
 sleep 1 &&
 mount -o remount,rw / &&
 sleep 1 &&
-wpa_passphrase $SSID $PW > /etc/wpa_supplicant.conf && 
+echo -e $CONFIG > /etc/wpa_supplicant.conf &&
 sleep 1 && 
-ifconfig eth0 $IP && 
+ifconfig eth0 $IP netmask $MASK && 
 sleep 1 &&
 mount -o remount,rw / &&
 sleep 1 &&
-wpa_passphrase $SSID $PW > /etc/wpa_supplicant.conf && 
+echo -e $CONFIG > /etc/wpa_supplicant.conf && 
 sleep 1 && 
-ifconfig eth0 $IP &&
+ifconfig eth0 $IP netmask $MASK && 
 sleep 1 &&
 wpa_supplicant -B -D wext -i eth0 -c /etc/wpa_supplicant.conf &&
 sleep 1 &&
 BLDC_Test_Bench -M 2 >/dev/null
 sleep 1 &&
 BLDC_Test_Bench -M 2 >/dev/null
-#wait 5 &&
-#/sbin/udhcpc -i eth0 
+# sleep 5 &&
